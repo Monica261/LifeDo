8aae246a773c1ff219bca4effbdef709514b8c7a
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-06-10 09:33:48 -0300

    atualizando todo o projeto

--- mobile/src/components/TaskCard/styles.js
+++ mobile/src/components/TaskCard/styles.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    borderRadius: 5
+    borderRadius: 10
   },
   cardLeft: {
     flexDirection: 'row',
